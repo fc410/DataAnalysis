a89e828525ff763a6ab60a5dd93fd0f2052d48cd
fc410 <54721672+fc410@users.noreply.github.com>
2020-03-17 15:16:42 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+#pip install matplotlib
+#python -m pip install --upgrade pip
+#pip install jupyter
+#pip install pandas
+#pip install matplotlib
+#pip install numpy
+#pip install seaborn
+#pip install sklearn
+#pip install pandas_datareader
