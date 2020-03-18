7ea483c291045d5aaacfbf5ab923e32698be101a
fc410 <54721672+fc410@users.noreply.github.com>
2020-03-18 16:12:06 -0700

    Real world problems

---
+++ realWorld.py
@@ -0,0 +1,8 @@
+#py -m venv venv
+#venv/Scripts/Activate
+#pip install pandas
+#pip install matplotlib
+#pip install jupyter
+#python -m pip install --upgrade pip
+#jupyter notebook
+#pip install sklearn
