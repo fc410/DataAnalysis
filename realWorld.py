a5670248aa07b9282a8982600b48ff924ca0ade5
fc410 <54721672+fc410@users.noreply.github.com>
2021-10-04 17:14:25 -0700

    Delete realWorld.py

--- realWorld.py
+++
@@ -1,8 +0,0 @@
-#py -m venv venv
-#venv/Scripts/Activate
-#pip install pandas
-#pip install matplotlib
-#pip install jupyter
-#python -m pip install --upgrade pip
-#jupyter notebook
-#pip install sklearn
